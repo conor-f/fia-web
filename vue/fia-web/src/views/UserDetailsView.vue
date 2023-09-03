ef2b0fca72825c5b12086f23d4d9aa2cb79f0d4c
Conor Flynn <conor.j.flynn@gmail.com>
2023-09-03 23:28:28 +0200

    Adds logout.

--- vue/fia-web/src/views/UserDetailsView.vue
+++ vue/fia-web/src/views/UserDetailsView.vue
@@ -3,11 +3,27 @@
     <h1>This is a User Details page</h1>
 
     You've logged in {{ timesLoggedIn }} times.
+
+    <input
+      type="button"
+      @click="handleLogoutClick"
+      value="Logout"
+      />
+    <input
+      type="button"
+      @click="handleDeleteClick"
+      value="Delete Account"
+      />
+
   </div>
 </template>
 
 <script lang="ts">
 import { getUserDetails } from "@/utils/api"
+import { toast } from "vue-sonner"
+import { useAuthStore } from "@/stores/authStore"
+
+const authStore = useAuthStore()
 
 
 export default {
@@ -25,6 +41,14 @@ export default {
       .catch(error => {
         console.log(error);
       });
+  },
+  methods: {
+    handleLogoutClick() {
+      authStore.accessToken = null;
+      authStore.refreshToken = null;
+      this.$router.push({ path: "/" });
+      toast.success("Logout successful");
+    },
   }
 }
 </script>
