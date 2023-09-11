ca92c9dd47175f1da793af9b16f228ca5b242621
Conor Flynn <conor.j.flynn@gmail.com>
2023-09-11 21:21:39 +0200

    Adds Vuestic

--- vue/fia-web/src/main.ts
+++ vue/fia-web/src/main.ts
@@ -2,6 +2,9 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuestic, createIconsConfig } from 'vuestic-ui';
+import "vuestic-ui/css";
+import "material-design-icons-iconfont/dist/material-design-icons.min.css";
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +13,9 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+// Check here for Vuestic style configuration. e.g. colour classes.
+// https://ui.vuestic.dev/getting-started/configuration-guide
+app.use(createVuestic())
 
 app.mount('#app')
