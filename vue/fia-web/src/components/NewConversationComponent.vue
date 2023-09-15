428559587107cd6f80b50c5fc9427269bbde930c
Conor Flynn <conor.j.flynn@gmail.com>
2023-09-15 18:07:50 +0200

    Supports audio input

--- vue/fia-web/src/components/NewConversationComponent.vue
+++ vue/fia-web/src/components/NewConversationComponent.vue
@@ -1,92 +1,3 @@
-<script lang="ts">
-import { getConversation, converse, converseWithAudio } from "@/utils/api"
-import VueVoiceRecording from "vue-voice-recording";
-import 'vue-voice-recording/dist/style.css';
-import { toast } from "vue-sonner"
-
-import ConversationLine from "@/components/ConversationLine.vue"
-
-import { ref, computed, onMounted } from "vue";
-import { useRecorder } from "vue-voice-recording";
-
-const {
-  isRecording,
-  toggleStartAndStop,
-} = useRecorder({
-  afterStartRecording: () => console.log("a"),
-  afterStopRecording: (blob) => console.log("b"),
-  afterPauseRecording: () => console.log("c"),
-  afterResumeRecording: () => console.log("d"),
-  getAsMp3: (value) => console.log("e"),
-});
-
-
-const conversation = ref([]);
-const userMessage = ref("");
-const conversation_id = ref("new");
-const response_loading = ref(false);
-
-const isActiveConversation = computed(() => {
-    return conversation_id.value != "new";
-})
-
-function handleConversationInput() {
-  response_loading.value = true;
-
-  const messageCopy = userMessage.value.slice();
-  userMessage.value = "";
-
-  // @ts-ignore
-  this.conversation.value.push({
-    role: "user",
-    message: messageCopy
-  });
-
-
-  converse(conversation_id, messageCopy)
-    .then(response => {
-      conversation_id.value = response.data.conversation_id
-      // @ts-ignore
-      conversation.value[conversation.value.length - 1]["learning_moments"] = response.data.learning_moments.learning_moments
-
-      // @ts-ignore
-      conversation.value.push({
-        role: "system",
-        message: response.data.conversation_response,
-      });
-
-      response_loading.value = false;
-    });
-}
-
-
-function handlAudioInput(audioInput) {
-  response_loading.value = true;
-
-  converseWithAudio(conversation_id, audioInput.data)
-    .then(response => {
-      conversation_id.value = response.data.conversation_id
-
-      // @ts-ignore
-      conversation.value.push({
-        role: "user",
-        message: response.data.input_message,
-      });
-
-      // @ts-ignore
-      conversation.value[conversation.value.length - 1]["learning_moments"] = response.data.learning_moments.learning_moments
-
-      // @ts-ignore
-      conversation.value.push({
-        role: "system",
-        message: response.data.conversation_response,
-      });
-
-      response_loading.value = false;
-    });
-}
-</script>
-
 <template>
   <div class="conversation">
     <div v-if="! isActiveConversation">
@@ -114,25 +25,106 @@ function handlAudioInput(audioInput) {
         @click="handleConversationInput"
         value="Send"
       />
-      <VueVoiceRecording @getAsMp3="gotMP3"></VueVoiceRecording>
     </va-inner-loading>
   </div>
-  <slot
-      :isRecording="isRecording"
-      :isPaused="isPaused"
-      :recordingTime="recordingTime"
-      :recordingState="recordingState"
-      :toggleStartAndStop="toggleStartAndStop"
-      :togglePauseAndResume="togglePauseAndResume"
-      :startRecording="startRecording"
-      :stopRecording="stopRecording"
-      :pauseRecording="pauseRecording"
-      :resumeRecording="resumeRecording"
-  >
-    <span v-if="isRecording">Rec</span>
-    <span v-else>Not</span>
-  </slot>
+
+  <div class="voice-recorder">
+    <va-icon name="mic" @click="toggleStartAndStop"/>
+    <span v-if="isRecording">
+      <svg height="100" width="100" class="blinking">
+        <circle cx="50" cy="50" r="10" fill="red" />
+        Sorry, your browser does not support inline SVG.
+      </svg>
+    </span>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import { getConversation, converse, converseWithAudio } from "@/utils/api"
+import { toast } from "vue-sonner"
+
+import ConversationLine from "@/components/ConversationLine.vue"
+
+import { ref, computed, onMounted } from "vue";
+// @ts-ignore
+import { useRecorder } from "vue-voice-recording";
+
+const {
+  isRecording,
+  toggleStartAndStop,
+} = useRecorder({
+  // @ts-ignore
+  getAsMp3: (value) => handleAudioInput(value),
+});
+
+
+const conversation = ref([]);
+const userMessage = ref("");
+const conversation_id = ref("new");
+const response_loading = ref(false);
+
+const isActiveConversation = computed(() => {
+    return conversation_id.value != "new";
+})
+
+function handleConversationInput() {
+  response_loading.value = true;
+
+  const messageCopy = userMessage.value.slice();
+  userMessage.value = "";
+
+  // @ts-ignore
+  this.conversation.value.push({
+    role: "user",
+    message: messageCopy
+  });
+
+
+  // @ts-ignore
+  converse(conversation_id, messageCopy)
+    .then(response => {
+      conversation_id.value = response.data.conversation_id
+      // @ts-ignore
+      conversation.value[conversation.value.length - 1]["learning_moments"] = response.data.learning_moments.learning_moments
+
+      // @ts-ignore
+      conversation.value.push({
+        role: "system",
+        message: response.data.conversation_response,
+      });
+
+      response_loading.value = false;
+    });
+}
+
+
+// @ts-ignore
+function handleAudioInput(audioInput) {
+  response_loading.value = true;
+
+  converseWithAudio(conversation_id.value, audioInput.data)
+    .then(response => {
+      conversation_id.value = response.data.conversation_id
+
+      // @ts-ignore
+      conversation.value.push({
+        role: "user",
+        message: response.data.input_message,
+      });
+
+      // @ts-ignore
+      conversation.value[conversation.value.length - 1]["learning_moments"] = response.data.learning_moments.learning_moments
+
+      // @ts-ignore
+      conversation.value.push({
+        role: "system",
+        message: response.data.conversation_response,
+      });
+
+      response_loading.value = false;
+    });
+}
+</script>
 
 <style scoped>
 textarea {
@@ -144,4 +136,59 @@ textarea {
   width: 100px;
   margin: auto auto;
 }
+
+/* BEGIN Record animation */
+.blinking {
+  -webkit-animation: 1s blink ease infinite;
+  -moz-animation: 1s blink ease infinite;
+  -ms-animation: 1s blink ease infinite;
+  -o-animation: 1s blink ease infinite;
+  animation: 1s blink ease infinite;
+}
+
+@keyframes "blink" {
+  from, to {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+}
+
+@-moz-keyframes blink {
+  from, to {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+}
+
+@-webkit-keyframes "blink" {
+  from, to {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+}
+
+@-ms-keyframes "blink" {
+  from, to {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+}
+
+@-o-keyframes "blink" {
+  from, to {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+}
+/* END Record animation */
 </style>
