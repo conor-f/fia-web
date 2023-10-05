3ee3196d77a04f6d35bce0cadef24247940cd0d6
Conor Flynn <conor.j.flynn@gmail.com>
2023-10-05 20:09:35 +0200

    Adds Responsive Homepage.

---
+++ vue/fia-web/src/components/ResponsiveHomepage.vue
@@ -0,0 +1,64 @@
+<template>
+  <div
+    class="
+    w-full
+    px-5 pt-7
+    text-xl text-background-50 font-bold text-center"
+    >
+    Fia gets you speaking and understanding a new language from day one
+  </div>
+
+  <Logo
+    class="
+    w-full
+    flex-initial basis-1/4 p-12 rounded-lg max-h-96 align-middle"
+    />
+
+  <div class="w-full px-5 text-l text-background-50">
+  Fia is an AI-powered language learning tool that allows you to 
+  have a conversation at your own level
+  </div>
+
+  <div class="w-full pt-5 px-5 text-l text-background-50">
+  Fia shows you your mistakes, and explains them to you
+  </div>
+
+  <div class="w-full pt-5 px-5 text-l text-background-50">
+  These mistakes are stored as flashcards, allowing you to review and improve every day
+  </div>
+
+  <div
+    class="w-full px-5 pt-7 text-xl text-background-50 font-bold text-center"
+  >
+    <button
+      class="
+        py-1 px-2 rounded
+        font-bold
+        bg-background-50 text-text-700
+        hover:bg-background-200 hover:text-text-900"
+        >
+      <RouterLink to="/login">
+          Register
+      </RouterLink>
+    </button>
+    to get started, or head over to our 
+    <button
+      class="
+        py-1 px-2 rounded
+        font-bold
+        bg-background-50 text-text-700
+        hover:bg-background-200 hover:text-text-900"
+      >
+      <RouterLink to="/about">
+          About
+      </RouterLink>
+    </button>
+    page for more information
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+
+import Logo from '@/assets/icons/Logo.vue'
+</script>
--- vue/fia-web/src/views/FiaHomepage.vue
+++ vue/fia-web/src/views/FiaHomepage.vue
@@ -1,34 +1,19 @@
 <template>
-  <!--
-    TODO:
-      - Have row text alternate left/right
-    -->
-  <div class="flex flex-col min-h-screen
-  bg-gradient-to-br from-red-500 to-orange-500">
-    <div class="flex flex-row justify-between">
-      <h1 class="flex-1 basis-2/4 text-4xl font-bold">
-        Fia gets you speaking and understanding a new language from day one.
-      </h1>
+  <div
+    class="flex flex-col min-h-screen
+    bg-gradient-to-br from-red-500 to-orange-500"
+    >
 
-      <div class="flex-initial basis-1/4"></div>
-
-      <!-- With this visible, there's padding on the right on small screens...  -->
-      <Logo
-        class="hidden flex-initial basis-1/4 p-12 rounded-lg max-h-96 align-middle shadow-2xl"
-        />
+    <div class="md:hidden">
+      <ResponsiveHomepage />
     </div>
 
-    <div
-      class="flex flex-row basis-2/4"
-      >
-      Fia is an AI-powered language learning tool.
-      You type (or speak!) to have a conversation at your own level.
-      Fia shows you your mistakes, and explains the corrected sentence.
-      All of your mistakes are stored as flashcards, allowing you to review and improve every day.
+    <div class="hidden md:inline-flex">
+      <ResponsiveHomepage />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import Logo from '@/assets/icons/Logo.vue'
+import ResponsiveHomepage from "@/components/ResponsiveHomepage.vue"
 </script>
