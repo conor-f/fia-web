c94900ded4f9ea887985ea6f4893bda4310eb856
Conor Flynn <conor.j.flynn@gmail.com>
2023-10-07 16:27:42 +0200

    WIP Separate inputs to component.

--- vue/fia-web/src/components/ConversationInputs.vue
+++ vue/fia-web/src/components/ConversationInputs.vue
@@ -1,32 +1,41 @@
 <template>
-  <div 
-    class="conversation"
-    @mousedown="setCoords"
-    >
+  <div class="pt-4 px-4">
+    <div class="relative">
+      <textarea
+        class="w-full rounded-lg border border-gray-300 bg-white px-4 py-3
+        pr-10 focus:outline-none focus:ring-2 focus:ring-gray-700
+        focus:ring-offset-2 focus:ring-offset-gray-100"
+        placeholder="Hallo! Wie Geht's?"
+        v-model="userMessage"
+        :loading="response_loading"
+        :resize="false"
+        autofocus="true"
+        @keyup.enter="handleConversationInput"
+        >
+      </textarea>
 
-    <div v-if="! isActiveConversation">
-      <ConversationPrompts/>
-      <ConversationInputs/>
+      <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+        <svg class="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+        </svg>
+      </div>
     </div>
 
-    <div v-else>
-      <ConversationLine
-        v-for="conversationLine in conversation"
-        :item="conversationLine"
-        :key="conversationLine"
-        />
+    <div class="buttons-container">
+      <div class="mic-container">
+        <va-icon name="mic" size="2rem" @click="toggleStartAndStop"/>
+          <span v-if="isRecording">
+            <svg height="80" width="80" class="blinking">
+              <circle cx="50" cy="50" r="10" fill="red" />
+              Sorry, your browser does not support inline SVG.
+            </svg>
+          </span>
+      </div>
+      <va-button @click="handleConversationInput">
+        Send
+      </va-button>
     </div>
   </div>
-
-  <TranslationPopup
-    v-if="shouldShowTranslation"
-    :selected-text="selectedText.text.value"
-    :xPosition="xCursorPosition"
-    :yPosition="yCursorPosition"
-    :conversation-ID="conversation_id"
-    @completed="completeTranslation"
-    >
-  </TranslationPopup>
 </template>
 
 <script lang="ts" setup>
@@ -41,8 +50,6 @@ import { ref, computed } from "vue";
 // @ts-ignore
 import { useRecorder } from "vue-voice-recording";
 
-import { useTextSelection } from '@vueuse/core';
-
 const {
   isRecording,
   toggleStartAndStop,
@@ -55,47 +62,6 @@ const conversation = ref([]);
 const userMessage = ref("");
 const conversation_id = ref("new");
 const response_loading = ref(false);
-const selectedText = useTextSelection()
-const xCursorPosition = ref(0)
-const yCursorPosition = ref(0)
-
-const conversationPrompts = ref([
-  {
-    "title": "Game",
-    "message": "Können wir zwanzig Fragen spielen?",
-  },
-  {
-    "title": "Sport",
-    "message": "Ich glaube, dass Irland die Rugby-Weltmeisterschaft gewinnen wird",
-  },
-  {
-    "title": "Pop Culture",
-    "message": "Glaubst du, Taylor Swift hat einen Bauchnabel?",
-  },
-  {
-    "title": "Help!",
-    "message": "Kannst du mir beibringen, wie man Kaffee in einer Moka-Kanne kocht?",
-  },
-]);
-
-const isActiveConversation = computed(() => {
-    return conversation_id.value != "new";
-})
-
-const shouldShowTranslation = computed(() => {
-  return isActiveConversation.value && selectedText.text.value != "";
-})
-
-function completeTranslation() {
-  // @ts-ignore
-  window.getSelection().removeAllRanges();
-}
-
-// @ts-ignore
-function setCoords(event) {
-  xCursorPosition.value = event.clientX;
-  yCursorPosition.value = event.clientY;
-}
 
 function handleConversationInput(event: any) {
   // shift + enter is common for new line.
