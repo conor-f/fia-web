e62e65e1b157b9e128edb85a197f4ed48c11709d
Conor Flynn <conor.j.flynn@gmail.com>
2023-10-14 02:58:38 +0200

    Adds options for languages.

--- vue/fia-web/src/views/UserDetailsView.vue
+++ vue/fia-web/src/views/UserDetailsView.vue
@@ -4,6 +4,15 @@
       You've logged in {{ timesLoggedIn }} times.
     </div>
 
+    <select class="select select-bordered w-ful max-w-xs">
+      <option
+        v-for="(language, index) in languageOptions"
+        :key="language.languageCode"
+        :value="language.languageCode"
+        >
+        {{ language.languageWord }}
+      </option>
+    </select>
     <div class="flex flex-row gap-2">
       <button
         class="btn btn-primary"
@@ -22,49 +31,74 @@
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
 import { getUserDetails, deleteAccount } from "@/utils/api"
 import { useToast } from "vue-toastification"
 import { useAuthStore } from "@/stores/authStore"
 
+import { useRouter } from "vue-router";
+
 const authStore = useAuthStore()
+const router = useRouter();
 const toast = useToast();
 
+const timesLoggedIn = ref(0);
+const languageOptions = ref([
+  {
+    language_code: "de",
+    languageWord: "German",
+    selected: false
+  },
+  {
+    languageCode: "it",
+    languageWord: "Italian",
+    selected: false
+  },
+  {
+    language_code: "es",
+    languageWord: "Spanish",
+    selected: false
+  },
+  {
+    language_code: "nl",
+    languageWord: "Dutch",
+    selected: false
+  },
+  {
+    language_code: "fr",
+    languageWord: "French",
+    selected: false
+  },
+]);
 
-export default {
-  name: 'UserDetailsView',
-  data() {
-    return {
-      "timesLoggedIn": 0,
-    }
-  },
-  created() {
-    getUserDetails()
-      .then(response => {
-        this.timesLoggedIn = response["data"]["times_logged_in"];
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  },
-  methods: {
-    handleLogoutClick() {
+onMounted(() => {
+  getUserDetails()
+    .then(response => {
+      timesLoggedIn.value = response["data"]["times_logged_in"];
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+function handleLogoutClick() {
+  authStore.clearTokens();
+  router.push({ path: "/" });
+  toast.success("Logout successful");
+}
+
+function handleDeleteClick() {
+  deleteAccount()
+    .then(response => {
       authStore.clearTokens();
-      this.$router.push({ path: "/" });
-      toast.success("Logout successful");
-    },
-    handleDeleteClick() {
-      deleteAccount()
-        .then(response => {
-          authStore.clearTokens();
-          toast.success("Account deleted successfully");
-          this.$router.push({ path: "/" });
-        })
-        .catch(error => {
-          console.log(error);
-          toast.error("Account deletion failed. Let me know!");
-        });
-    },
-  }
+      toast.success("Account deleted successfully");
+      router.push({ path: "/" });
+    })
+    .catch(error => {
+      console.log(error);
+      toast.error("Account deletion failed. Let me know!");
+    });
 }
 </script>
