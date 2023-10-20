09b525230df6fe674496bd34c6da80f5f8b274f9
Conor Flynn <conor-f@users.noreply.github.com>
2023-10-20 18:14:31 +0200

    Supports different language prompts. (#36)
    * Supports different language prompts.
    
    * Fixes TS errors.

---
+++ vue/fia-web/src/utils/prompts.ts
@@ -0,0 +1,94 @@
+const prompts = {
+  de: [
+    {
+      "title": "Game",
+      "prompt": "Können wir zwanzig Fragen spielen?",
+    },
+    {
+      "title": "Sport",
+      "prompt": "Ich glaube, dass Irland die Rugby-Weltmeisterschaft gewinnen wird",
+    },
+    {
+      "title": "Pop Culture",
+      "prompt": "Glaubst du, Taylor Swift hat einen Bauchnabel?",
+    },
+    {
+      "title": "Help!",
+      "prompt": "Kannst du mir beibringen, wie man Kaffee in einer Moka-Kanne kocht?",
+    },
+  ],
+  it: [
+    {
+      "title": "Game",
+      "prompt": "Possiamo giocare a venti domande?",
+    },
+    {
+      "title": "Sport",
+      "prompt": "Qual è lo sport più seguito in Italia?",
+    },
+    {
+      "title": "Pop Culture",
+      "prompt": "Cantami una strofa di una canzone pop italiana!",
+    },
+    {
+      "title": "Help!",
+      "prompt": "La mia carbonara dovrebbe contenere aglio?",
+    },
+  ],
+  es: [
+    {
+      "title": "Game",
+      "prompt": "Juguemos a veinte preguntas!",
+    },
+    {
+      "title": "Sport",
+      "prompt": "¿Qué son los juegos tradicionales españoles?",
+    },
+    {
+      "title": "Pop Culture",
+      "prompt": "¿Quién es la música española más famosa?",
+    },
+    {
+      "title": "Food",
+      "prompt": "¿Es realmente sabrosa la paella?",
+    },
+  ],
+  fr: [
+    {
+      "title": "Game",
+      "prompt": "Jouons à vingt questions !",
+    },
+    {
+      "title": "Sport",
+      "prompt": "Pourquoi Zidane a-t-il donné un coup de tête à Materazzi ?",
+    },
+    {
+      "title": "Pop Culture",
+      "prompt": "Quelle musique est la plus populaire en France ?",
+    },
+    {
+      "title": "Food",
+      "prompt": "Est-ce difficile de faire une baguette ?",
+    },
+  ],
+  nl: [
+    {
+      "title": "Game",
+      "prompt": "Laten we twintig vragen spelen!",
+    },
+    {
+      "title": "Sport",
+      "prompt": "Wat is de meest traditionele Nederlandse sport?",
+    },
+    {
+      "title": "Pop Culture",
+      "prompt": "Kunnen Nederlanders rappen?",
+    },
+    {
+      "title": "Food",
+      "prompt": "Wat hebben Nederlanders als toetje?",
+    },
+  ],
+}
+
+export default prompts;
